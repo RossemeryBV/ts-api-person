437d14c859e652f5a6c8c59b4fb342d761ae4f54
Rossemery <Rossemery@Memy.mshome.net>
2019-03-24 13:19:04 -0600

    Para que se inicie solo

--- src/main/java/com/person/BackendApplication.java
+++ src/main/java/com/person/BackendApplication.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
